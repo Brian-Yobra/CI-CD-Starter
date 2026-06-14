4706126944d679a7d4346592c6221efd33a81074
Brian Kihara <britzpuler@gmail.com>
2026-06-14 19:15:26 +0300

    Added Style Tests

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth.js";
 
-describe('Api-Key', () => {
+describe("Api-Key", () => {
   test("Api-key 1", () => {
     expect(getAPIKey({ authorization: "ApiKey hello" })).toBe("hello");
-  })
+  });
   test("Api-key Erroed", () => {
     expect(getAPIKey({})).toBe(null);
-  })
+  });
   test("Api-key Empty Field", () => {
     expect(getAPIKey({ authorization: "" })).toBe(null);
-  })
+  });
 });
